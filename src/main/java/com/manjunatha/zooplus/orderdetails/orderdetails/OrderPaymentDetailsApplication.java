90fff9aad90ca2727d88c98d9152ea65203ef17c
ManjuGitHub847302 <manjunatha.mvit@gmail.com>
2022-02-22 04:09:37 +0530

    First commit 2022-02-22

---
+++ src/main/java/com/manjunatha/zooplus/orderdetails/orderdetails/OrderPaymentDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.manjunatha.zooplus.orderdetails.orderdetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderPaymentDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderPaymentDetailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manjunatha/zooplus/orderdetails/orderdetails/OrderPaymentDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manjunatha.zooplus.orderdetails.orderdetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderPaymentDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
